fccb1122bc078f8d92dd5cae718f5854fe79dc20
AllanPettigrew <allanpettigrew1@gmail.com>
2018-03-07 09:27:49 -0800

    Added Index, CSS, js, SCSS

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// Author Cory LaViska / Smooth scroll
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
